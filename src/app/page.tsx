3e1a4d8915ca2991bbd1b501b32dc49446c28a1c
lasting <lastingcoder@qq.com>
2023-03-02 10:04:36 +0800

    add(dependency): aloliasearch, clsx

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Home() {
-  return (
-    <div className=" text-sky-400">
-      <Link href={"/home"}>home</Link>
-    </div>
-  );
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mask: "rgba(0,0,0,0.2)",
+      },
+    },
   },
   plugins: [],
 };
