8d7a40dbf726cd055e91b006a1b876c6e2900921
lasting <lastingcoder@qq.com>
2023-03-04 13:57:06 +0800

    UI: search button combine with doc search modal UI: doc search modal auto focus

--- src/ui/DocSearchModal.tsx
+++ src/ui/DocSearchModal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import clsx from "clsx";
-import { useEffect, useState } from "react";
+import { MouseEventHandler, useEffect, useRef } from "react";
 import EscapeButton from "./EscapeButton";
 import SearchSvg from "./SearchSvg";
 
@@ -12,17 +12,32 @@ export default function DocSearchModal({
   modalOpen: boolean;
   setModalOpen: (f: boolean) => void;
 }) {
+  const handleMaskClick: MouseEventHandler<HTMLDivElement> = (ev) => {
+    ev.stopPropagation();
+  };
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (modalOpen) {
+      inputRef.current?.focus();
+    }
+  }, [modalOpen]);
+
   return (
     <div
       className={clsx(
-        "doc-search-container w-screen h-screen backdrop-blur-sm bg-mask absolute flex justify-center",
+        "doc-search-container w-screen h-screen backdrop-blur-sm bg-mask absolute top-0 flex justify-center z-20",
         { hidden: !modalOpen }
       )}
+      onClick={handleMaskClick}
     >
       <div className="doc-search-modal w-3/5 dark:text-slate-400 bg-white dark:bg-slate-800 dark:ring-slate-900 overflow-hidden h-96 rounded-xl p-6 ring-1 mt-24 ring-slate-200">
         <header className=" flex items-center">
           <SearchSvg />
           <input
+            ref={inputRef}
+            maxLength={48}
             className=" outline-none mx-2 flex-1 dark:bg-slate-800"
             placeholder="Search document..."
           />
--- src/ui/SearchButton.tsx
+++ src/ui/SearchButton.tsx
@@ -1,22 +1,52 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
+import DocSearchModal from "./DocSearchModal";
 import SearchSvg from "./SearchSvg";
 
-export default function SearchButton({
-  onClick,
-}: {
-  onClick: React.DOMAttributes<HTMLButtonElement>["onClick"];
-}) {
+export default function SearchButton() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("keydown", (ev) => {
+      if ((ev.ctrlKey || ev.metaKey) && ev.key.toLowerCase() === "k") {
+        setModalOpen(true);
+      } else if (ev.key === "Escape") {
+        setModalOpen(false);
+      }
+    });
+
+    return () => {
+      document.removeEventListener("keydown", (ev) => {
+        if ((ev.ctrlKey || ev.metaKey) && ev.key.toLowerCase() === "k") {
+          setModalOpen(true);
+        } else if (ev.key === "Escape") {
+          setModalOpen(false);
+        }
+      });
+    };
+  }, []);
+
   return (
-    <button
-      onClick={onClick}
-      className=" hover:ring-slate-400 hover:dark:bg-slate-600 outline-none overflow-hidden w-52 rounded-lg h-9 bg-white ring-1 ring-slate-200 dark:ring-slate-900 dark:bg-slate-800 flex items-center"
-    >
-      <SearchSvg />
-      <span className="ml-2 text-sm">Quick search...</span>
-      <span className="ml-auto mr-3 pl-3 flex-none text-xs font-semibold">
-        ⌘K
-      </span>
-    </button>
+    <>
+      <button
+        onClick={() => {
+          setModalOpen(true);
+        }}
+        className=" hover:ring-slate-400 hover:dark:bg-slate-600 outline-none overflow-hidden w-52 rounded-lg h-9 bg-white ring-1 ring-slate-200 dark:ring-slate-900 dark:bg-slate-800 flex items-center"
+      >
+        <SearchSvg />
+        <span className="ml-2 text-sm">Quick search...</span>
+        <span className="ml-auto mr-3 pl-3 flex-none text-xs font-semibold">
+          ⌘K
+        </span>
+      </button>
+      {modalOpen &&
+        createPortal(
+          <DocSearchModal modalOpen={modalOpen} setModalOpen={setModalOpen} />,
+          document.body
+        )}
+    </>
   );
 }
